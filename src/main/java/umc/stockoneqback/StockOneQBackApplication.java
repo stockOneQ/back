e3f769c9446b7f98a356c4cf0da15f292af09f79
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2023-07-10 00:08:59 +0900

    init: init setup for spring framework on github

---
+++ src/main/java/umc/stockoneqback/StockOneQBackApplication.java
@@ -0,0 +1,11 @@
+package umc.stockoneqback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockOneQBackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StockOneQBackApplication.class, args);
+    }
+}
---
+++ src/test/java/umc/stockoneqback/StockOneQBackApplicationTests.java
@@ -0,0 +1,11 @@
+package umc.stockoneqback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockOneQBackApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
