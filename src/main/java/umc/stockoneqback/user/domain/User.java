d74337de4d6ebc53ccd7fb05a9c17eed7c4c991a
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2023-07-12 23:24:23 +0900

    fix: user 엔티티의 테이블명 users로 변경

--- src/main/java/umc/stockoneqback/user/domain/User.java
+++ src/main/java/umc/stockoneqback/user/domain/User.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
