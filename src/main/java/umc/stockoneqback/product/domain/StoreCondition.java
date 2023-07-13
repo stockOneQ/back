107719ac5c43d42788e28412affeba373191327b
길지운 <wldns2577@khu.ac.kr>
2023-07-14 02:18:40 +0900

    feat: Product Entity 생성

---
+++ src/main/java/umc/stockoneqback/product/domain/StoreCondition.java
@@ -0,0 +1,24 @@
+package umc.stockoneqback.product.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import umc.stockoneqback.global.EnumConverter;
+import umc.stockoneqback.global.EnumStandard;
+
+@Getter
+@AllArgsConstructor
+public enum StoreCondition implements EnumStandard {
+
+    FREEZING("냉동"),
+    REFRIGERATING("냉장"),
+    ROOM("상온");
+
+    private String value;
+
+    @javax.persistence.Converter
+    public static class Converter extends EnumConverter<StoreCondition> {
+        public Converter() {
+            super(StoreCondition.class);
+        }
+    }
+}
