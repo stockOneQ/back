45b979725910083cbeae56b79cc6f5eb2e64fff8
길지운 <wldns2577@khu.ac.kr>
2023-07-23 06:02:37 +0900

    refactor: 통일된 규칙 적용을 위한 ProductController 클래스명 변경

--- src/main/java/umc/stockoneqback/product/controller/ProductApiController.java
+++ src/main/java/umc/stockoneqback/product/controller/ProductApiController.java
@@ -17,7 +17,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/product")
-public class ProductController {
+public class ProductApiController {
     private final ProductService productService;
 
     @PostMapping("/add")
