0fd1cc21a80b3c093c41f3f48122f498b422232c
길지운 <wldns2577@khu.ac.kr>
2023-07-22 23:17:04 +0900

    feat: 기존의 실행 안되는 query fix 및 제품 목록 조회를 위한 query 추가

--- src/main/java/umc/stockoneqback/product/domain/ProductRepository.java
+++ src/main/java/umc/stockoneqback/product/domain/ProductRepository.java
@@ -3,7 +3,6 @@ package umc.stockoneqback.product.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import umc.stockoneqback.product.dto.response.SearchProductUrl;
 import umc.stockoneqback.role.domain.store.Store;
 
 import java.time.LocalDate;
@@ -11,36 +10,114 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    @Query("SELECT p.id, p.name, p.imageUrl FROM Product p WHERE p.status = 'NORMAL' AND p.store = :store" +
-            "AND p.storeCondition = :storeCondition AND p.name LIKE %:name% ORDER BY p.name")
-    List<SearchProductUrl> findProductByName(@Param("store") Store store,
-                                             @Param("storeCondition") StoreCondition storeCondition,
+    @Query(value = "SELECT p.* FROM Product p WHERE p.status = '정상' AND p.store = :store " +
+            "AND p.store_condition = :storeCondition AND p.name LIKE %:name% ORDER BY p.name", nativeQuery = true)
+    List<Product> findProductByName(@Param("store") Store store,
+                                             @Param("storeCondition") String storeCondition,
                                              @Param("name") String productName);
-    @Query("SELECT 1 FROM Product p WHERE p.status = 'NORMAL' AND p.store = :store" +
-            "AND p.storeCondition = :storeCondition AND p.name = :name LIMIT 1")
-    Integer isExistProductByName(@Param("store") Store store,
-                                 @Param("storeCondition") StoreCondition storeCondition,
+    @Query(value = "SELECT p.* FROM Product p WHERE p.status = '정상' AND p.store = :store " +
+            "AND p.store_condition = :storeCondition AND p.name = :name", nativeQuery = true)
+    Optional<Product> isExistProductByName(@Param("store") Store store,
+                                 @Param("storeCondition") String storeCondition,
                                  @Param("name") String productName);
 
-    @Query("SELECT p FROM Product p WHERE p.id = :id AND p.status = 'NORMAL'")
+    @Query(value = "SELECT p.* FROM Product p WHERE p.id = :id AND p.status = '정상'", nativeQuery = true)
     Optional<Product> findProductById(@Param("id") Long productId);
 
-    @Query("SELECT count(*) FROM Product p WHERE p.store = :store AND p.storeCondition = :storeCondition AND p.status = 'NORMAL'")
+    @Query(value = "SELECT count(*) FROM Product p WHERE p.store = :store " +
+            "AND p.store_condition = :storeCondition AND p.status = '정상'", nativeQuery = true)
     Integer countProductAll(@Param("store") Store store,
-                            @Param("storeCondition") StoreCondition storeCondition);
-    @Query("SELECT count(*) FROM Product p WHERE p.store = :store AND p.storeCondition = :storeCondition" +
-            "AND p.expirationDate < :currentDate AND p.status = 'NORMAL'")
+                            @Param("storeCondition") String storeCondition);
+    @Query(value = "SELECT count(*) FROM Product p WHERE p.store = :store AND p.store_condition = :storeCondition " +
+            "AND p.expiration_date < :currentDate AND p.status = '정상'", nativeQuery = true)
     Integer countProductPass(@Param("store") Store store,
-                             @Param("storeCondition") StoreCondition storeCondition,
+                             @Param("storeCondition") String storeCondition,
                              @Param("currentDate") LocalDate currentDate);
-    @Query("SELECT count(*) FROM Product p WHERE p.store = :store AND p.storeCondition = :storeCondition" +
-            "AND (p.expirationDate >= :currentDate AND p.expirationDate <= :standardDate) AND p.status = 'NORMAL'")
+    @Query(value = "SELECT count(*) FROM Product p WHERE p.store = :store AND p.store_condition = :storeCondition " +
+            "AND (p.expiration_date >= :currentDate AND p.expiration_date <= :standardDate) AND p.status = '정상'", nativeQuery = true)
     Integer countProductClose(@Param("store") Store store,
-                              @Param("storeCondition") StoreCondition storeCondition,
+                              @Param("storeCondition") String storeCondition,
                               @Param("currentDate") LocalDate currentDate,
                               @Param("standardDate") LocalDate standardDate);
-    @Query("SELECT count(*) FROM Product p WHERE p.store = :store AND p.storeCondition = :storeCondition" +
-            "AND p.requireQuant > p.stockQuant AND p.status = 'NORMAL'")
+    @Query(value = "SELECT count(*) FROM Product p WHERE p.store = :store AND p.store_condition = :storeCondition " +
+            "AND p.require_quant > p.stock_quant AND p.status = '정상'", nativeQuery = true)
     Integer countProductLack(@Param("store") Store store,
-                             @Param("storeCondition") StoreCondition storeCondition);
+                             @Param("storeCondition") String storeCondition);
+
+    @Query(value = "SELECT p.* FROM Product p WHERE p.store = :store AND p.store_condition = :storeCondition " +
+            "AND p.status = '정상' AND (p.name > :name OR :name is null) ORDER BY p.name LIMIT :pageSize", nativeQuery = true)
+    List<Product> findPageOfAllOrderByName(@Param("store") Store store,
+                                                     @Param("storeCondition") String storeCondition,
+                                                     @Param("name") String productName,
+                                                     @Param("pageSize") Integer pageSize);
+
+    @Query(value = "SELECT p.* FROM Product p WHERE p.store = :store AND p.store_condition = :storeCondition " +
+            "AND p.status = '정상' AND ((:orderFreq is null AND :name is null) OR " +
+            "(p.order_freq < :orderFreq OR p.order_freq = :orderFreq AND p.name > :name)) " +
+            "ORDER BY p.order_freq DESC, p.name LIMIT :pageSize", nativeQuery = true)
+    List<Product> findPageOfAllOrderByOrderFreq(@Param("store") Store store,
+                                                     @Param("storeCondition") String storeCondition,
+                                                     @Param("name") String productName,
+                                                     @Param("orderFreq") Long orderFreq,
+                                                     @Param("pageSize") Integer pageSize);
+
+    @Query(value = "SELECT p.* FROM Product p WHERE p.store = :store AND p.store_condition = :storeCondition " +
+            "AND p.status = '정상' AND p.expiration_date < :currentDate " +
+            "AND (p.name > :name OR :name is null) ORDER BY p.name LIMIT :pageSize", nativeQuery = true)
+    List<Product> findPageOfPassOrderByName(@Param("store") Store store,
+                                                    @Param("storeCondition") String storeCondition,
+                                                    @Param("name") String productName,
+                                                    @Param("pageSize") Integer pageSize,
+                                                     @Param("currentDate") LocalDate currentDate);
+
+    @Query(value = "SELECT p.* FROM Product p WHERE p.store = :store AND p.store_condition = :storeCondition " +
+            "AND p.status = '정상' AND p.expiration_date < :currentDate " +
+            "AND ((p.order_freq < :orderFreq OR p.order_freq = :orderFreq AND p.name > :name) " +
+            "OR (:orderFreq is null AND :name is null)) ORDER BY p.order_freq DESC, p.name LIMIT :pageSize", nativeQuery = true)
+    List<Product> findPageOfPassOrderByOrderFreq(@Param("store") Store store,
+                                                         @Param("storeCondition") String storeCondition,
+                                                         @Param("name") String productName,
+                                                         @Param("orderFreq") Long orderFreq,
+                                                         @Param("pageSize") Integer pageSize,
+                                                          @Param("currentDate") LocalDate currentDate);
+
+    @Query(value = "SELECT p.* FROM Product p WHERE p.store = :store AND p.store_condition = :storeCondition " +
+            "AND p.status = '정상' AND (p.expiration_date >= :currentDate AND p.expiration_date <= :standardDate) " +
+            "AND (p.name > :name OR :name is null) ORDER BY p.name LIMIT :pageSize", nativeQuery = true)
+    List<Product> findPageOfCloseOrderByName(@Param("store") Store store,
+                                                    @Param("storeCondition") String storeCondition,
+                                                    @Param("name") String productName,
+                                                    @Param("pageSize") Integer pageSize,
+                                                    @Param("currentDate") LocalDate currentDate,
+                                                    @Param("standardDate") LocalDate standardDate);
+
+    @Query(value = "SELECT p.* FROM Product p WHERE p.store = :store AND p.store_condition = :storeCondition " +
+            "AND p.status = '정상' AND (p.expiration_date >= :currentDate AND p.expiration_date <= :standardDate) " +
+            "AND ((p.order_freq < :orderFreq OR p.order_freq = :orderFreq AND p.name > :name) " +
+            "OR (:orderFreq is null AND :name is null)) ORDER BY p.order_freq DESC, p.name LIMIT :pageSize", nativeQuery = true)
+    List<Product> findPageOfCloseOrderByOrderFreq(@Param("store") Store store,
+                                                       @Param("storeCondition") String storeCondition,
+                                                       @Param("name") String productName,
+                                                       @Param("orderFreq") Long orderFreq,
+                                                       @Param("pageSize") Integer pageSize,
+                                                       @Param("currentDate") LocalDate currentDate,
+                                                       @Param("standardDate") LocalDate standardDate);
+
+    @Query(value = "SELECT p.* FROM Product p WHERE p.store = :store AND p.store_condition = :storeCondition " +
+            "AND p.status = '정상' AND p.require_quant > p.stock_quant " +
+            "AND (p.name > :name OR :name is null) ORDER BY p.name LIMIT :pageSize", nativeQuery = true)
+    List<Product> findPageOfLackOrderByName(@Param("store") Store store,
+                                                    @Param("storeCondition") String storeCondition,
+                                                    @Param("name") String productName,
+                                                    @Param("pageSize") Integer pageSize);
+
+    @Query(value = "SELECT p.* FROM Product p WHERE p.store = :store AND p.store_condition = :storeCondition " +
+            "AND p.status = '정상' AND p.require_quant > p.stock_quant " +
+            "AND ((p.order_freq < :orderFreq OR p.order_freq = :orderFreq AND p.name > :name) " +
+            "OR (:orderFreq is null AND :name is null)) ORDER BY p.order_freq DESC, p.name LIMIT :pageSize", nativeQuery = true)
+    List<Product> findPageOfLackOrderByOrderFreq(@Param("store") Store store,
+                                                         @Param("storeCondition") String storeCondition,
+                                                         @Param("name") String productName,
+                                                         @Param("orderFreq") Long orderFreq,
+                                                         @Param("pageSize") Integer pageSize);
 }
