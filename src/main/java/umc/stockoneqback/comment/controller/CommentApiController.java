1d6da4f85ef50dd6df6739febe9e64ff11657baa
SunKyoung Yoon <yskkjdh@gmail.com>
2023-07-27 01:39:36 +0900

    refactor: 댓글, 대댓글 writerId @PathVariable -> @ExtractPayload 수정

--- src/main/java/umc/stockoneqback/comment/controller/CommentApiController.java
+++ src/main/java/umc/stockoneqback/comment/controller/CommentApiController.java
@@ -5,31 +5,32 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import umc.stockoneqback.comment.controller.dto.CommentRequest;
 import umc.stockoneqback.comment.service.CommentService;
+import umc.stockoneqback.global.annotation.ExtractPayload;
 
 import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/comments/{writerId}")
+@RequestMapping("/api/comments")
 public class CommentApiController {
     private final CommentService commentService;
 
     @PostMapping("/{boardId}")
-    public ResponseEntity<Void> create(@PathVariable Long writerId, @PathVariable Long boardId,
+    public ResponseEntity<Void> create(@ExtractPayload Long writerId, @PathVariable Long boardId,
                                        @RequestBody @Valid CommentRequest request) {
         commentService.create(writerId, boardId, request.image(), request.content());
         return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/{commentId}")
-    public ResponseEntity<Void> update(@PathVariable Long writerId, @PathVariable Long commentId,
+    public ResponseEntity<Void> update(@ExtractPayload Long writerId, @PathVariable Long commentId,
                                        @RequestBody @Valid CommentRequest request) {
         commentService.update(writerId, commentId, request.image(), request.content());
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{commentId}")
-    public ResponseEntity<Void> delete(@PathVariable Long writerId, @PathVariable Long commentId) {
+    public ResponseEntity<Void> delete(@ExtractPayload Long writerId, @PathVariable Long commentId) {
         commentService.delete(writerId, commentId);
         return ResponseEntity.ok().build();
     }
--- src/main/java/umc/stockoneqback/reply/controller/ReplyApiController.java
+++ src/main/java/umc/stockoneqback/reply/controller/ReplyApiController.java
@@ -3,6 +3,7 @@ package umc.stockoneqback.reply.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import umc.stockoneqback.global.annotation.ExtractPayload;
 import umc.stockoneqback.reply.controller.dto.ReplyRequest;
 import umc.stockoneqback.reply.service.ReplyService;
 
@@ -10,26 +11,26 @@ import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/replies/{writerId}")
+@RequestMapping("/api/replies")
 public class ReplyApiController {
     private final ReplyService replyService;
 
     @PostMapping("/{commentId}")
-    public ResponseEntity<Void> create(@PathVariable Long writerId, @PathVariable Long commentId,
+    public ResponseEntity<Void> create(@ExtractPayload Long writerId, @PathVariable Long commentId,
                                        @RequestBody @Valid ReplyRequest request) {
         replyService.create(writerId, commentId, request.image(), request.content());
         return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/{replyId}")
-    public ResponseEntity<Void> update(@PathVariable Long writerId, @PathVariable Long replyId,
+    public ResponseEntity<Void> update(@ExtractPayload Long writerId, @PathVariable Long replyId,
                                        @RequestBody @Valid ReplyRequest request) {
         replyService.update(writerId, replyId, request.image(), request.content());
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{replyId}")
-    public ResponseEntity<Void> delete(@PathVariable Long writerId, @PathVariable Long replyId) {
+    public ResponseEntity<Void> delete(@ExtractPayload Long writerId, @PathVariable Long replyId) {
         replyService.delete(writerId, replyId);
         return ResponseEntity.ok().build();
     }
