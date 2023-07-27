0088f71167a208cb8409f1825ac9361b3ae11b6b
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2023-07-28 02:24:28 +0900

    feat: store fixture 추가

--- src/test/java/umc/stockoneqback/fixture/StoreFixture.java
+++ src/test/java/umc/stockoneqback/fixture/StoreFixture.java
@@ -9,11 +9,13 @@ import umc.stockoneqback.role.domain.store.Store;
 public enum StoreFixture {
     Z_YEONGTONG("Z 과일가게 영통점", "과일", "경기도 수원시 영통구 에듀타운로 65"),
     Z_SIHEUNG("Z 과일가게 시흥점", "과일", "경기도 시흥시 은계남로 12"),
-    Y_YEONGTONG("Y 빵집 영통점", "빵", "경기도 수원시 영통구 영통로 195");
+    Y_YEONGTONG("Y 빵집 영통점", "빵", "경기도 수원시 영통구 영통로 195"),
+    A_PASTA("A 파스타집 행당점", "파스타", "서울특별시 성동구 행당로 33"),
+    B_CHICKEN("B 치킨집 강남점", "치킨", "서울특별시 강남구 강남대로 123"),
+    ;
+
     private final String name;
-
     private final String sector;
-
     private final String address;
 
     public Store toStore() {
