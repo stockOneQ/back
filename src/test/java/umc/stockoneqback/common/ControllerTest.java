f06506d61a3a307e1dcb27f66ceeeb9bf57b5eda
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2023-07-28 00:24:07 +0900

    [Docs] Spring Rest Docs를 이용하여 API 명세서를 작성한다 (#64)
    * refactor: optimize imports
    
    * test: TokenReissueApiController에 대한 테스트 코드 작성
    
    * fix: build failed 해결을 위해 Lombok 버전 변경
    
    * docs: rest docs 추출

--- src/test/java/umc/stockoneqback/common/ControllerTest.java
+++ src/test/java/umc/stockoneqback/common/ControllerTest.java
@@ -15,7 +15,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import umc.stockoneqback.auth.controller.AuthApiController;
+import umc.stockoneqback.auth.controller.TokenReissueApiController;
 import umc.stockoneqback.auth.service.AuthService;
+import umc.stockoneqback.auth.service.TokenReissueService;
 import umc.stockoneqback.auth.utils.JwtTokenProvider;
 import umc.stockoneqback.board.controller.BoardApiController;
 import umc.stockoneqback.board.controller.like.BoardLikeApiController;
@@ -59,7 +61,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
         ProductApiController.class,
         FriendApiController.class,
         FriendFindApiController.class,
-        BoardLikeApiController.class
+        BoardLikeApiController.class,
+        TokenReissueApiController.class
 })
 @ExtendWith(RestDocumentationExtension.class)
 @AutoConfigureRestDocs
@@ -129,6 +132,9 @@ public abstract class ControllerTest {
   
     @MockBean
     protected BoardLikeService boardLikeService;
+
+    @MockBean
+    protected TokenReissueService tokenReissueService;
 
     @BeforeEach
     void setUp(WebApplicationContext context, RestDocumentationContextProvider provider) {
