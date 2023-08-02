9c1fd4afc1eec934e4eca48bb9c53e0fbd364372
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2023-08-02 21:52:50 +0900

    feat: 사용자 정보 조회 API 구현

--- src/main/java/umc/stockoneqback/user/controller/UserInformationApiController.java
+++ src/main/java/umc/stockoneqback/user/controller/UserInformationApiController.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import umc.stockoneqback.global.annotation.ExtractPayload;
 import umc.stockoneqback.user.controller.dto.request.FindLoginIdRequest;
 import umc.stockoneqback.user.domain.Email;
 import umc.stockoneqback.user.service.UserInformationService;
 import umc.stockoneqback.user.service.dto.response.LoginIdResponse;
+import umc.stockoneqback.user.service.dto.response.UserInformationResponse;
 
 import javax.validation.Valid;
 
@@ -22,6 +24,12 @@ public class UserInformationApiController {
     @GetMapping("/find-id")
     public ResponseEntity<LoginIdResponse> findLoginId(@RequestBody @Valid FindLoginIdRequest request) {
         LoginIdResponse response = userInformationService.findLoginId(request.name(), request.birth(), Email.from(request.email()));
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/information")
+    public ResponseEntity<UserInformationResponse> getInformation(@ExtractPayload Long userId) {
+        UserInformationResponse response = userInformationService.getInformation(userId);
         return ResponseEntity.ok(response);
     }
 }
--- src/main/java/umc/stockoneqback/user/service/UserInformationService.java
+++ src/main/java/umc/stockoneqback/user/service/UserInformationService.java
@@ -4,10 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import umc.stockoneqback.global.base.BaseException;
+import umc.stockoneqback.role.domain.store.PartTimer;
+import umc.stockoneqback.role.domain.store.PartTimerRepository;
 import umc.stockoneqback.user.domain.Email;
+import umc.stockoneqback.user.domain.Role;
 import umc.stockoneqback.user.domain.User;
 import umc.stockoneqback.user.exception.UserErrorCode;
 import umc.stockoneqback.user.service.dto.response.LoginIdResponse;
+import umc.stockoneqback.user.service.dto.response.UserInformationResponse;
 
 import java.time.LocalDate;
 
@@ -16,6 +20,7 @@ import java.time.LocalDate;
 @RequiredArgsConstructor
 public class UserInformationService {
     private final UserFindService userFindService;
+    private final PartTimerRepository partTimerRepository;
 
     public LoginIdResponse findLoginId(String name, LocalDate birth, Email email) {
         User user = userFindService.findByEmail(email);
@@ -24,5 +29,18 @@ public class UserInformationService {
         }
 
         return new LoginIdResponse(user.getLoginId());
+    }
+
+    public UserInformationResponse getInformation(Long userId) {
+        User user = userFindService.findById(userId);
+
+        if (user.getRole().equals(Role.MANAGER)) {
+            return new UserInformationResponse(user.getId(), user.getEmail().getValue(), user.getLoginId(), user.getName(), user.getBirth(), user.getPhoneNumber(), user.getRole().getAuthority(), user.getManagerStore().getName(), user.getManagerStore().getCode(), user.getManagerStore().getAddress(), null, null);
+        } else if (user.getRole().equals(Role.PART_TIMER)) {
+            PartTimer partTimer = partTimerRepository.findByPartTimer(user).orElseThrow(() -> BaseException.type(UserErrorCode.USER_NOT_FOUND));
+            return new UserInformationResponse(user.getId(), user.getEmail().getValue(), user.getLoginId(), user.getName(), user.getBirth(), user.getPhoneNumber(), user.getRole().getAuthority(), partTimer.getStore().getName(), null, partTimer.getStore().getAddress(), null, null);
+        } else {
+            return new UserInformationResponse(user.getId(), user.getEmail().getValue(), user.getLoginId(), user.getName(), user.getBirth(), user.getPhoneNumber(), user.getRole().getAuthority(), null, null, null, user.getCompany().getName(), null);
+        }
     }
 }
