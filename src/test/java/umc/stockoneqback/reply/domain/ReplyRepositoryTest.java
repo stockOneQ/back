5068a8da1d70c9601a0dfc1b4fb44d8129dae942
길지운 <wldns2577@khu.ac.kr>
2023-08-13 02:29:15 +0900

    test: 회원 탈퇴 로직에 필요한 Repository 쿼리 테스트 코드 작성

--- src/test/java/umc/stockoneqback/reply/domain/ReplyRepositoryTest.java
+++ src/test/java/umc/stockoneqback/reply/domain/ReplyRepositoryTest.java
@@ -1,6 +1,7 @@
 package umc.stockoneqback.reply.domain;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import umc.stockoneqback.board.domain.Board;
@@ -17,6 +18,7 @@ import static umc.stockoneqback.fixture.CommentFixture.COMMENT_0;
 import static umc.stockoneqback.fixture.ReplyFixture.*;
 import static umc.stockoneqback.fixture.UserFixture.SAEWOO;
 
+@DisplayName("Reply [Repository Layer] -> ReplyRepository 테스트")
 public class ReplyRepositoryTest extends RepositoryTest {
     @Autowired
     private UserRepository userRepository;
@@ -30,12 +32,13 @@ public class ReplyRepositoryTest extends RepositoryTest {
     @Autowired
     private ReplyRepository replyRepository;
 
+    private User writer;
     private Comment comment;
     private final Reply[] replies = new Reply[3];
 
     @BeforeEach
     void setup() {
-        User writer = userRepository.save(SAEWOO.toUser());
+        writer = userRepository.save(SAEWOO.toUser());
         Board board = boardRepository.save(BOARD_0.toBoard(writer));
         comment = commentRepository.save(COMMENT_0.toComment(writer, board));
         replies[0] = replyRepository.save(REPLY_0.toReply(writer, comment));
@@ -44,8 +47,19 @@ public class ReplyRepositoryTest extends RepositoryTest {
     }
 
     @Test
+    @DisplayName("댓글의 대댓글 수를 가져온다")
     void countByBoard() {
         assertThat(replyRepository.countByComment(comment)).isEqualTo(3L);
+    }
+
+    @Test
+    @DisplayName("회원 ID를 통해 해당 회원의 대댓글을 모두 삭제한다")
+    void deleteByUser() {
+        // when
+        replyRepository.deleteByWriter(writer);
+
+        // then
+        assertThat(replyRepository.findAll().isEmpty()).isTrue();
     }
 }
 
---
+++ src/test/java/umc/stockoneqback/share/domain/ShareRepositoryTest.java
@@ -0,0 +1,53 @@
+package umc.stockoneqback.share.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import umc.stockoneqback.business.domain.Business;
+import umc.stockoneqback.business.domain.BusinessRepository;
+import umc.stockoneqback.common.RepositoryTest;
+import umc.stockoneqback.share.repository.ShareRepository;
+import umc.stockoneqback.user.domain.User;
+import umc.stockoneqback.user.domain.UserRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static umc.stockoneqback.fixture.ShareFixture.*;
+import static umc.stockoneqback.fixture.UserFixture.ANNE;
+import static umc.stockoneqback.fixture.UserFixture.WIZ;
+
+@DisplayName("Share [Repository Layer] -> ShareRepository 테스트")
+public class ShareRepositoryTest extends RepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BusinessRepository businessRepository;
+
+    @Autowired
+    private ShareRepository shareRepository;
+
+    private User manager;
+    private User supervisor;
+    private Business business;
+
+    @BeforeEach
+    void setup() {
+        manager = userRepository.save(ANNE.toUser());
+        supervisor = userRepository.save(WIZ.toUser());
+        business = businessRepository.save(Business.builder().manager(manager).supervisor(supervisor).build());
+        shareRepository.save(SHARE_0.toShare(business));
+        shareRepository.save(SHARE_1.toShare(business));
+        shareRepository.save(SHARE_2.toShare(business));
+    }
+
+    @Test
+    @DisplayName("회원 ID를 통해 해당 회원과 관련된 커넥트 게시글을 모두 삭제한다")
+    void deleteByUser() {
+        // when
+        shareRepository.deleteByBusiness(business);
+
+        // then
+        assertThat(shareRepository.findAll().isEmpty()).isTrue();
+    }
+}
