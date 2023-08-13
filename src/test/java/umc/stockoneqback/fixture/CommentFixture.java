05758cd87adf6a048448068dc8f7978e41b6b9fe
SunKyoung Yoon <yskkjdh@gmail.com>
2023-08-13 14:25:47 +0900

    refactor: 댓글, 대댓글 목록 테스트를 위해 추가 작성

--- src/test/java/umc/stockoneqback/fixture/CommentFixture.java
+++ src/test/java/umc/stockoneqback/fixture/CommentFixture.java
@@ -9,9 +9,21 @@ import umc.stockoneqback.user.domain.User;
 @Getter
 @RequiredArgsConstructor
 public enum CommentFixture {
-    COMMENT_0("이미지0", "댓글0"),
-    COMMENT_1("이미지1", "댓글1"),
-    COMMENT_2("이미지2", "댓글2")
+    COMMENT_0(null, "댓글0"),
+    COMMENT_1(null, "댓글1"),
+    COMMENT_2(null, "댓글2"),
+    COMMENT_3(null, "댓글3"),
+    COMMENT_4(null, "댓글4"),
+    COMMENT_5(null, "댓글5"),
+    COMMENT_6(null, "댓글6"),
+    COMMENT_7(null, "댓글7"),
+    COMMENT_8(null, "댓글8"),
+    COMMENT_9(null, "댓글9"),
+    COMMENT_10(null, "댓글10"),
+    COMMENT_11(null, "댓글11"),
+    COMMENT_12(null, "댓글12"),
+    COMMENT_13(null, "댓글13"),
+    COMMENT_14(null, "댓글14")
     ;
 
     private final String image;
--- src/test/java/umc/stockoneqback/fixture/ReplyFixture.java
+++ src/test/java/umc/stockoneqback/fixture/ReplyFixture.java
@@ -9,9 +9,16 @@ import umc.stockoneqback.user.domain.User;
 @Getter
 @RequiredArgsConstructor
 public enum ReplyFixture {
-    REPLY_0("이미지0", "대댓글0"),
-    REPLY_1("이미지1", "대댓글1"),
-    REPLY_2("이미지2", "대댓글2")
+    REPLY_0(null, "대댓글0"),
+    REPLY_1(null, "대댓글1"),
+    REPLY_2(null, "대댓글2"),
+    REPLY_3(null, "대댓글3"),
+    REPLY_4(null, "대댓글4"),
+    REPLY_5(null, "대댓글5"),
+    REPLY_6(null, "대댓글6"),
+    REPLY_7(null, "대댓글7"),
+    REPLY_8(null, "대댓글8"),
+    REPLY_9(null, "대댓글9")
     ;
 
     private final String image;
