8ffbff9baee1ecc43c94627ff7e092f7d2195567
SunKyoung Yoon <yskkjdh@gmail.com>
2023-08-13 14:28:03 +0900

    feat: 댓글, 대댓글 전체 목록 조회 api 구현

---
+++ src/main/java/umc/stockoneqback/comment/controller/dto/CustomCommentListResponse.java
@@ -0,0 +1,30 @@
+package umc.stockoneqback.comment.controller.dto;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+public record CustomCommentListResponse<C>(
+        CustomPageable pageInfo,
+
+        List<CommentListResponse> CommentListResponse
+
+) {
+    @Getter
+    @NoArgsConstructor
+    public static class CustomPageable{
+        long totalPages;
+        long totalElements;
+        boolean hasNext;
+        long numberOfElements;
+
+        public CustomPageable(long totalPages, long totalElements, boolean hasNext, long numberOfElements) {
+            this.totalPages = totalPages;
+            this.totalElements = totalElements;
+            this.hasNext = hasNext;
+            this.numberOfElements = numberOfElements;
+        }
+    }
+}
