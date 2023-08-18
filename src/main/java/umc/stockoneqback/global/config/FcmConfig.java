f6e5d83acf1611a9f72c15edfa75ad14bcca0845
길지운 <90232934+Ji-Un-Gil@users.noreply.github.com>
2023-08-19 07:02:01 +0900

    [Chore] Firebase의 설정 파일 형식을 변경한다 (#155)
    - chore: firebase 설정파일을 json에서 yml로 변경함에 따른 gitignore 수정 및 fcm 설정 시 형변환 로직 추가
    
    Co-authored-by: 길지운 <wldns2577@khu.ac.kr>

--- src/main/java/umc/stockoneqback/global/config/FcmConfig.java
+++ src/main/java/umc/stockoneqback/global/config/FcmConfig.java
@@ -4,27 +4,31 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.gson.JsonObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.yaml.snakeyaml.Yaml;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Map;
 
 @Configuration
 public class FcmConfig {
     @Bean
     public FirebaseMessaging initialize() throws IOException {
-        // initialize Admin SDK using OAuth 2.0 refresh fcmToken
-        ClassPathResource resource = new ClassPathResource("/firebase/firebase_service_secret.json");
-        InputStream refreshToken = resource.getInputStream();
+        ClassPathResource resource = new ClassPathResource("/firebase/firebase_service_secret.yml");
+        InputStream inputStream = resource.getInputStream();
+        InputStream refreshToken = convertYmlToJson(inputStream);
 
         FirebaseApp firebaseApp = null;
         List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
 
         if (firebaseAppList != null && !firebaseAppList.isEmpty()) {
-            for (FirebaseApp app: firebaseAppList) {
+            for (FirebaseApp app : firebaseAppList) {
                 if (app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
                     firebaseApp = app;
                     break;
@@ -39,5 +43,17 @@ public class FcmConfig {
         }
 
         return FirebaseMessaging.getInstance(firebaseApp);
+    }
+
+    private InputStream convertYmlToJson(InputStream inputStream) {
+        Yaml yaml = new Yaml();
+        Map<String, Object> configMap = yaml.load(inputStream);
+
+        JsonObject jsonConfig = new JsonObject();
+        for (Map.Entry<String, Object> entry : configMap.entrySet()) {
+            jsonConfig.addProperty(entry.getKey(), entry.getValue().toString());
+        }
+
+        return new ByteArrayInputStream(jsonConfig.toString().getBytes());
     }
 }
