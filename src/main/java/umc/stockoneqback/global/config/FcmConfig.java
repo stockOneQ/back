e0f121ba9d4da8b59b1c4831082f0625935791c7
길지운 <wldns2577@khu.ac.kr>
2023-08-20 19:01:36 +0900

    chore: firebase 설정을 application.yml에 병합 및 FcmConfig 수정

--- src/main/java/umc/stockoneqback/global/config/FcmConfig.java
+++ src/main/java/umc/stockoneqback/global/config/FcmConfig.java
@@ -5,24 +5,48 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.gson.JsonObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.yaml.snakeyaml.Yaml;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
-import java.util.Map;
 
 @Configuration
 public class FcmConfig {
+    @Value("${TYPE}")
+    private String type;
+    @Value("${PROJECT_ID}")
+    private String projectId;
+    @Value("${PRIVATE_KEY_ID}")
+    private String privateKeyId;
+    @Value("${PRIVATE_KEY}")
+    private String privateKey;
+    @Value("${CLIENT_EMAIL}")
+    private String clientEmail;
+    @Value("${CLIENT_ID}")
+    private String clientId;
+    @Value("${AUTH_URI}")
+    private String authUri;
+    @Value("${TOKEN_URI}")
+    private String tokenUri;
+    @Value("${AUTH_PROVIDER_X509_CERT_URL}")
+    private String authProviderCertUrl;
+    @Value("${CLIENT_X509_CERT_URL}")
+    private String clientCertUrl;
+    @Value("${UNIVERSE_DOMAIN}")
+    private String universeDomain;
+
+    private final List<String> keys = List.of("type", "project_id", "private_key_id", "private_key",
+            "client_email", "client_id", "auth_uri", "token_uri", "auth_provider_x509_cert_url", "client_x509_cert_url", "universe_domain");
+    private final List<String> values = List.of(type, projectId, privateKeyId, privateKey, clientEmail, clientId, authUri,
+            tokenUri, authProviderCertUrl, clientCertUrl, universeDomain);
+
     @Bean
     public FirebaseMessaging initialize() throws IOException {
-        ClassPathResource resource = new ClassPathResource("/firebase/firebase_service_secret.yml");
-        InputStream inputStream = resource.getInputStream();
-        InputStream refreshToken = convertYmlToJson(inputStream);
+        InputStream refreshToken = convertYmlToJson();
 
         FirebaseApp firebaseApp = null;
         List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
@@ -45,15 +69,10 @@ public class FcmConfig {
         return FirebaseMessaging.getInstance(firebaseApp);
     }
 
-    private InputStream convertYmlToJson(InputStream inputStream) {
-        Yaml yaml = new Yaml();
-        Map<String, Object> configMap = yaml.load(inputStream);
-
+    private InputStream convertYmlToJson() {
         JsonObject jsonConfig = new JsonObject();
-        for (Map.Entry<String, Object> entry : configMap.entrySet()) {
-            jsonConfig.addProperty(entry.getKey(), entry.getValue().toString());
-        }
-
+        for (int i = 0; i < keys.size(); i++)
+            jsonConfig.addProperty(keys.get(i), values.get(i));
         return new ByteArrayInputStream(jsonConfig.toString().getBytes());
     }
 }
